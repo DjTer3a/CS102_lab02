460c2dede30a7f3d8e4efc28ab69c1c9b2cbbefa
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-22 17:50:25 +0400

    part d done(minor edit & no comments)

--- src/MySimpleURLReader.java
+++ src/MySimpleURLReader.java
@@ -85,7 +85,8 @@ public static void main(String[] args){
         MySimpleURLReader obj = new MySimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/scrapethissite.html");
         System.out.println(obj.getUrl());
         System.out.println(obj.getName());
-        System.out.print(obj.getUnfilteredPageContents());
+        System.out.print(obj.getPageContents());
+
         System.out.println(obj.getLineCount());
         System.out.println(obj.getNumberOfCSSLinks());
     }
