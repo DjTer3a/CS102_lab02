8c28c231c7adc609952c59cdb706bb87dbabed5e
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-22 15:33:58 +0400

    part b done (no comments)

--- src/MySimpleURLReader.java
+++ src/MySimpleURLReader.java
@@ -16,6 +16,20 @@ public class MySimpleURLReader extends SimpleURLReader{
         return url;
     }
 
+    public String getName(){
+        int idx = url.lastIndexOf('/');
+        int len = url.length();
+        String name;
+        name =url.substring(idx+1, len);
+        return name;
+    }
+
+    public String getPageContents(){
+        String pageContent;
+        pageContent = super.getPageContents();
+        return pageContent;
+    }
+
     public int getLineCount(){
         int lineCount;
         lineCount = super.getLineCount();
@@ -24,6 +38,9 @@ public class MySimpleURLReader extends SimpleURLReader{
     }   
 public static void main(String[] args){
         HtmlScraperTest obj = new HtmlScraperTest("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+        System.out.println(obj.getUrl());
+        System.out.println(obj.getName());
+        System.out.println(obj.getPageContents());
         System.out.println(obj.getLineCount());
     }
 }
