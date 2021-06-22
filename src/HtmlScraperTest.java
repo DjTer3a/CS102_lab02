0ce60ab5c79b72682439c02b838d507ef96e393b
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-22 18:24:30 +0400

    part d edits (no comments)

--- src/HtmlScraperTest.java
+++ src/HtmlScraperTest.java
@@ -1,9 +1,9 @@
 public class HtmlScraperTest extends MySimpleURLReader {
     public static void main(String[] args){
-            MySimpleURLReader obj = new MySimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+            MySimpleURLReader obj = new MySimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/scrapethissite.html");
             System.out.println(obj.getUrl());
             System.out.println(obj.getName());
-            System.out.println(obj.getPageContents());
+            System.out.println(obj.getUnfilteredPageContents());
             System.out.println(obj.getLineCount());
         }
 
