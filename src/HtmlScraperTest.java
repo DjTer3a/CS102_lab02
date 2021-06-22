04b1e2762ba68895ca35acffa1f962cd937245b0
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-22 14:30:22 +0400

    Created & modified HtmlScraper and SimpleUrlReader

---
+++ src/HtmlScraperTest.java
@@ -0,0 +1,47 @@
+import cs1.SimpleURLReader;
+
+public class HtmlScraperTest extends SimpleURLReader {
+    String url ="";
+
+
+    //Constructor
+    public HtmlScraperTest(String url){
+        super(url);
+        this.url =url;
+    }
+
+
+    //Methods
+    public String getUrl(){
+        return url;
+    }
+
+    public String getName(){
+        int idx = url.lastIndexOf('/');
+        int len = url.length();
+        String name;
+        name =url.substring(idx+1, len);
+        return name;
+    }
+
+    public String getPageContents(){
+        String pageContent;
+        pageContent = super.getPageContents();
+        return pageContent;
+    }
+
+    public int getLineCount(){
+        int lineCount;
+        lineCount = super.getLineCount();
+        
+        return lineCount;
+    }   
+public static void main(String[] args){
+        HtmlScraperTest obj = new HtmlScraperTest("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+        System.out.println(obj.getUrl());
+        System.out.println(obj.getName());
+        System.out.println(obj.getPageContents());
+        System.out.println(obj.getLineCount());
+    }
+
+}
