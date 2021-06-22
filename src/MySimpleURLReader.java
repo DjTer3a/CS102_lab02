beb2bba6ffac3caa386b7c23e9b3ff417c504e85
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-22 15:33:33 +0400

    part a done(no comments)

--- src/MySimpleURLReader.java
+++ src/MySimpleURLReader.java
@@ -1,3 +1,29 @@
-public class MySimpleURLReader {
-    
+import cs1.SimpleURLReader;
+
+public class MySimpleURLReader extends SimpleURLReader{
+    String url ="";
+
+
+    //Constructor
+    public MySimpleURLReader(String url){
+        super(url);
+        this.url =url;
+    }
+
+
+    //Methods
+    public String getUrl(){
+        return url;
+    }
+
+    public int getLineCount(){
+        int lineCount;
+        lineCount = super.getLineCount();
+        
+        return lineCount;
+    }   
+public static void main(String[] args){
+        HtmlScraperTest obj = new HtmlScraperTest("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+        System.out.println(obj.getLineCount());
+    }
 }
