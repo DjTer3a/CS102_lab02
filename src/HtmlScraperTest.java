25b63841b05ad279187a9912578a481586ad56a4
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-06-24 16:28:42 +0400

    part e done (no comments)

--- src/HtmlScraperTest.java
+++ src/HtmlScraperTest.java
@@ -1,10 +1,17 @@
-public class HtmlScraperTest extends MySimpleURLReader {
+import org.graalvm.compiler.word.WordOperationPlugin;
+
+public class HtmlScraperTest extends WordSearch {
     public static void main(String[] args){
-            MySimpleURLReader obj = new MySimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/scrapethissite.html");
-            System.out.println(obj.getUrl());
-            System.out.println(obj.getName());
-            System.out.println(obj.getUnfilteredPageContents());
-            System.out.println(obj.getLineCount());
+            // HTMLFilteredReader obj = new HTMLFilteredReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/scrapethissite.html");
+            // System.out.println(obj.getUrl());
+            // System.out.println(obj.getName());
+            // System.out.println(obj.getUnfilteredPageContents());
+            // System.out.println(obj.getLineCount());
+            // System.out.println(obj.getPageContents());
+
+            WordSearch obj2 = new WordSearch("http://www.cs.bilkent.edu.tr/~ozturk/cs102/scrapethissite.html");
+
+            obj2.check("Ankara");
         }
 
 }
---
+++ src/WordSearch.java
@@ -0,0 +1,32 @@
+
+public class WordSearch extends HTMLFilteredReader {
+        public WordSearch (String url){
+            super(url);
+        }
+   
+        public void check(String word){
+            String text = super.getPageContents();
+            int counter = 0;
+
+            if(text.contains(word)){
+                for(int i = 0;  i<(text.length()-word.length()); i++ ){
+                if(text.substring(i, i+word.length()).equals(word)){
+                    
+                    counter++;
+                    int idx = i;
+                    System.out.println("Found the word ("+word+") at the index "+ idx + " and ends at the index of "+ (idx+word.length())   );
+                }
+                }
+                System.out.println("The word was repeated this many times:-"+counter);
+            }
+            else{
+                System.out.println("Word not found in the text!!");
+            }
+        
+    
+        }
+
+ }
+
+    
+    
